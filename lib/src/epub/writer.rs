689069d26fd5a42174ecd521c7159a228fb23679
Ding <michael19950415@outlook.com>
2025-04-01 11:03:03 +0800

    fix

--- lib/src/epub/writer.rs
+++ lib/src/epub/writer.rs
@@ -17,7 +17,9 @@ impl From<zip::result::ZipError> for IError {
     fn from(value: zip::result::ZipError) -> Self {
         match value {
             zip::result::ZipError::Io(io) => IError::Io(io),
-            zip::result::ZipError::InvalidArchive(v) => IError::InvalidArchive(v),
+            zip::result::ZipError::InvalidArchive(v) => {
+                IError::InvalidArchive(Box::leak(v.into_owned().into_boxed_str()))
+            }
             zip::result::ZipError::UnsupportedArchive(v) => IError::UnsupportedArchive(v),
             zip::result::ZipError::InvalidPassword => IError::InvalidPassword,
             zip::result::ZipError::FileNotFound => IError::FileNotFound,
