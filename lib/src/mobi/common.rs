3b21aa523d31e8e48b746f6ccb52a982ad7c3b6b
inkbox <enpassPixiv@protonmail.com>
2025-07-29 11:53:13 +0800

    feat: 公开时间格式化

--- lib/src/mobi/common.rs
+++ lib/src/mobi/common.rs
@@ -248,9 +248,9 @@ pub(crate) struct INDXRecord {
 /// 格式化时间戳
 pub(crate) fn do_time_format(value: u32) -> String {
     if value & 0x80000000 == 0x80000000 {
-        crate::common::do_time_display((value & 0x7fffffff) as u64, 1904)
+        crate::common::DateTimeFormater::custom_start((value & 0x7fffffff) as u64, 1904).default_format()
     } else {
-        crate::common::time_display(value as u64)
+        crate::common::DateTimeFormater::new(value as u64).default_format()
     }
 }
 fn u8_to_string<const N: usize>(v: [u8; N]) -> String {
